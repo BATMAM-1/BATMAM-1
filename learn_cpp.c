940d127a5feaceee1a72a75c7590b831da1dc0ec
siddharthvarmaj <134192345+J-siddharth-varma@users.noreply.github.com>
2023-12-23 11:55:26 +0530

    Add files via upload
    new repo for learning

---
+++ learn_cpp.c
@@ -0,0 +1,42 @@
+
+// PROGRAM: TO FIND OUT IF A NUMBER IS EVEN OR ODD.//
+/*#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+
+    return 0;
+}*/
+
+// CREATE AND USE ENUM type//
+ 
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ /*int main()
+ {
+    enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT };
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("The value of xerox is :%d\n", xerox);
+    printf("the value of google is: %d\n", google);
+    printf("The vlaue of ebay is: %d\n", ebay);
+     
+ return 0;
+ }*/
+
+/*
+
+
+*/
+
+
